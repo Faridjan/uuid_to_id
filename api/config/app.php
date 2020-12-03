175ea5904576ddcb42f1d253b382bb887e957374
Farid <fred.andersen7@gmail.com>
2020-12-03 19:04:07 +0600

    Create App bundle

---
+++ api/config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return static function (\Psr\Container\ContainerInterface $container) {
+    $app = \Slim\Factory\AppFactory::createFromContainer($container);
+
+    (require __DIR__ . '/../config/middleware.php')($app, $container);
+    (require __DIR__ . '/../config/routes.php')($app);
+
+    return $app;
+};
--- api/public/index.php
+++ api/public/index.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 $container = require __DIR__ . '/../config/container.php';
-$app = \Slim\Factory\AppFactory::createFromContainer($container);
 
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-(require __DIR__ . '/../config/routes.php')($app);
+$app = (require __DIR__ . '/../config/app.php')($container);
 
 $app->run();
